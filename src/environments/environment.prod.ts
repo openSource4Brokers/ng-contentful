7b714c861438f3e94421f43ddf885d3d93f27fe6
Jos Vermoesen <info@rv.be>
2020-05-12 04:51:08 +0200

    shape up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,13 @@
 export const environment = {
   production: true,
-  contentful: {
+  /* contentful: {
     spaceId: 's83us0f2hupg',
     token: '7035396e8135606db666a2124fdf48af5b180d85f1e3469127957aa112154e1f'
+  } */
+
+  // vsoft!
+  contentful: {
+    spaceId: 'mq8ieqd7mcv8',
+    token: 'e92105b30fe907b0de47100961329d50bec5e0476f55473e1b821e4919e4a26e'
   }
 };
